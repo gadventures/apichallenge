5c7c91f5e95a240e02ff2baa0853efcc83033c41
abby <abby.greenbloom@gmail.com>
2016-05-11 16:02:45 -0400

    refactor view before putting views into classes

--- trips/urls.py
+++ trips/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
-
+from rest_framework.urlpatterns import format_suffix_patterns
+from trips import views
 from .views import TripView
 
 urlpatterns = [
-    url('^', TripView.as_view())
+    url('^', TripView.as_view()),
+    url(r'^trips/$', views.trip_list),
+    url(r'^snippets/(?P<pk>[0-9]+)$', views.trip_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
