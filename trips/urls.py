0940e165b59b8d7f7999871cc73c8a895a3d8913
abby <abby.greenbloom@gmail.com>
2016-05-12 14:42:23 -0400

    create initial ajax call on index page

--- trips/urls.py
+++ trips/urls.py
@@ -6,7 +6,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^trips-api/$', TripList.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^trips-api/(?P<pk>[0-9]+)/$', TripDetail.as_view()),
+    url(r'^trips-api/(?P<pk>[0-9]+)/$', TripDetail.as_view(), name='trip-detail'),
     url(r'^', TripView.as_view())
 ]
 
