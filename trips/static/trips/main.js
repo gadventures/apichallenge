5c124bfa5b507f69b6ae5adcb1f96d1b89b7f5b3
abby <abby.greenbloom@gmail.com>
2016-05-12 21:12:08 -0400

    add basic styles to view page

--- trips/static/trips/main.js
+++ trips/static/trips/main.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+
+  var today = new Date();
+  var dd = today.getDate();
+
   load_trips()
   function load_trips() {
     $.ajax({
@@ -6,7 +10,7 @@ $( document ).ready(function() {
       url: 'trips-api/',
       success: function(data) {
         for (var i = 0 ; i < data.length; i++) {
-          $('#results').append('<li id="trip-{data.id}">' + "<strong>" + data[i]['name'] +"</strong>" + " <br>  " + "<em> start date </em>" +  data[i]['start_date'] + "   " + "<em> end date </em> " + data[i]['finish_date'] + '</li>')
+          $('#results').append('<li id="trip-{data.id}" class="col-md-8 col-md-offset-2">' + "<strong>" + data[i]['name'] +"</strong>" + " <br>  " + "<em> start date </em>" +  data[i]['start_date'] + "   " + "<em> end date </em> " + data[i]['finish_date'] + '</li>')
         }
       },
       error: function() {
